fd4b513d750e08bac8e9e4d0de67a07fa8dd056f
Logan <tushlogan@gmail.com>
2024-07-09 11:53:16 +0100

    added tap to scroll

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -18,6 +18,7 @@ import consensys from "@/components/assets/consensys.png";
 import base from "@/components/assets/base.png";
 import ramp from "@/components/assets/ramp.png";
 import abstract from "@/components/assets/abstract.png";
+import { Link, Element } from "react-scroll";
 
 const fadeInUp = {
   hidden: { opacity: 0, y: 20 },
@@ -65,111 +66,115 @@ const Home = () => {
             className='bg-bv-blue-1 border border-white rounded-[100px] font-lato text-xl px-8 py-4 w-fit mx-auto'
             variants={fadeInUp}
           >
-            Enter the multiverse!
+            <Link to='main' spy={true} smooth={true} duration={500}>
+              Enter the multiverse!
+            </Link>
           </motion.button>
         </motion.div>
         <div className='mt-20'>
           <Image src={world} alt='Image' />
         </div>
       </section>
-      <section className='px-[22px] mt-[32px] lg:mt-[74px] bg-white text-white lg:px-[104px]'>
-        <motion.h1
-          className='font-clash text-[24px] lg:text-[48px] font-medium text-center text-bv-black-1'
-          initial='hidden'
-          whileInView='visible'
-          viewport={{ once: true }}
-          variants={fadeInUp}
-        >
-          Why Build With Blocverse?
-        </motion.h1>
-        <motion.div
-          className='mt-[40px] flex flex-col space-y-10'
-          initial='hidden'
-          whileInView='visible'
-          viewport={{ once: true }}
-          variants={stagger}
-        >
+      <Element name='main'>
+        <section className='px-[22px] mt-[32px] lg:mt-[74px] bg-white text-white lg:px-[104px]'>
+          <motion.h1
+            className='font-clash text-[24px] lg:text-[48px] font-medium text-center text-bv-black-1'
+            initial='hidden'
+            whileInView='visible'
+            viewport={{ once: true }}
+            variants={fadeInUp}
+          >
+            Why Build With Blocverse?
+          </motion.h1>
           <motion.div
-            className='grid grid-cols-1 lg:grid-cols-3 gap-10'
+            className='mt-[40px] flex flex-col space-y-10'
+            initial='hidden'
+            whileInView='visible'
+            viewport={{ once: true }}
             variants={stagger}
           >
             <motion.div
-              className='bg-bv-black-1 overflow-hidden h-[200px] lg:h-[300px] relative flex items-center justify-center rounded-2xl col-span-1 lg:col-span-2'
-              variants={fadeInUp}
+              className='grid grid-cols-1 lg:grid-cols-3 gap-10'
+              variants={stagger}
             >
-              <p className='font-clash z-10 text-[28px] max-w-[700px] lg:text-[56px] text-center leading-[34.44px] lg:leading-[68.88px]'>
-                Seamlessly integrate{" "}
-                <span className='text-bv-blue-2'>blockchain.</span>
-              </p>
-              <Image
-                className='absolute bottom-0'
-                src={seamlessImg}
-                alt='Image'
-              />
+              <motion.div
+                className='bg-bv-black-1 overflow-hidden h-[200px] lg:h-[300px] relative flex items-center justify-center rounded-2xl col-span-1 lg:col-span-2'
+                variants={fadeInUp}
+              >
+                <p className='font-clash z-10 text-[28px] max-w-[700px] lg:text-[56px] text-center leading-[34.44px] lg:leading-[68.88px]'>
+                  Seamlessly integrate{" "}
+                  <span className='text-bv-blue-2'>blockchain.</span>
+                </p>
+                <Image
+                  className='absolute bottom-0'
+                  src={seamlessImg}
+                  alt='Image'
+                />
+              </motion.div>
+              <motion.div
+                className='bg-bv-black-1 overflow-hidden relative rounded-2xl px-[29px] pt-[40px] h-[200px] lg:h-[300px]'
+                variants={fadeInUp}
+              >
+                <p className='font-clash text-[24px] leading-[29.52px] lg:text-[32px] lg:leading-[39.36px]'>
+                  Innovation to stay ahead of the competition.
+                </p>
+                <Image
+                  className='absolute -right-28 -bottom-28 lg:-right-8 lg:-bottom-8'
+                  src={innovationImg}
+                  alt='Image'
+                />
+              </motion.div>
             </motion.div>
             <motion.div
-              className='bg-bv-black-1 overflow-hidden relative rounded-2xl px-[29px] pt-[40px] h-[200px] lg:h-[300px]'
-              variants={fadeInUp}
+              className='grid grid-cols-1 lg:grid-cols-3 gap-10'
+              variants={stagger}
             >
-              <p className='font-clash text-[24px] leading-[29.52px] lg:text-[32px] lg:leading-[39.36px]'>
-                Innovation to stay ahead of the competition.
-              </p>
-              <Image
-                className='absolute -right-28 -bottom-28 lg:-right-8 lg:-bottom-8'
-                src={innovationImg}
-                alt='Image'
-              />
+              <motion.div
+                className='bg-bv-black-1 overflow-hidden relative rounded-2xl px-[29px] pt-[40px] h-[200px] lg:h-[300px]'
+                variants={fadeInUp}
+              >
+                <p className='font-clash z-10 text-[24px] leading-[29.52px] lg:text-[32px] lg:leading-[39.36px]'>
+                  Dedicated partners on your{" "}
+                  <span className='text-bv-blue-2'>journey</span> to success.
+                </p>
+                <Image
+                  className='absolute -right-28 -bottom-28 lg:-right-8 lg:-bottom-8'
+                  src={journeyImg}
+                  alt='Image'
+                />
+              </motion.div>
+              <motion.div
+                className='bg-bv-black-1 overflow-hidden relative rounded-2xl px-[29px] pt-[40px] h-[200px] lg:h-[300px]'
+                variants={fadeInUp}
+              >
+                <p className='font-clash text-[24px] leading-[29.52px] lg:text-[32px] lg:leading-[39.36px]'>
+                  Explore the endless possibilities of your business or idea.
+                </p>
+                <Image
+                  className='absolute -right-8 -bottom-8 lg:right-0 lg:bottom-0'
+                  src={exploreImg}
+                  alt='Image'
+                />
+              </motion.div>
+              <motion.div
+                className='bg-bv-black-1 overflow-hidden relative rounded-2xl px-[29px] pt-[40px] h-[200px] lg:h-[300px]'
+                variants={fadeInUp}
+              >
+                <p className='font-clash text-[24px] leading-[29.52px] lg:text-[32px] lg:leading-[39.36px]'>
+                  Build your MVP in 6 weeks.
+                </p>
+                <Image
+                  className='absolute -right-16 -bottom-24 lg:right-0 lg:bottom-0 lg:relative mx-auto'
+                  src={mvpImg}
+                  alt='Image'
+                  width={186}
+                  height={186}
+                />
+              </motion.div>
             </motion.div>
           </motion.div>
-          <motion.div
-            className='grid grid-cols-1 lg:grid-cols-3 gap-10'
-            variants={stagger}
-          >
-            <motion.div
-              className='bg-bv-black-1 overflow-hidden relative rounded-2xl px-[29px] pt-[40px] h-[200px] lg:h-[300px]'
-              variants={fadeInUp}
-            >
-              <p className='font-clash z-10 text-[24px] leading-[29.52px] lg:text-[32px] lg:leading-[39.36px]'>
-                Dedicated partners on your{" "}
-                <span className='text-bv-blue-2'>journey</span> to success.
-              </p>
-              <Image
-                className='absolute -right-28 -bottom-28 lg:-right-8 lg:-bottom-8'
-                src={journeyImg}
-                alt='Image'
-              />
-            </motion.div>
-            <motion.div
-              className='bg-bv-black-1 overflow-hidden relative rounded-2xl px-[29px] pt-[40px] h-[200px] lg:h-[300px]'
-              variants={fadeInUp}
-            >
-              <p className='font-clash text-[24px] leading-[29.52px] lg:text-[32px] lg:leading-[39.36px]'>
-                Explore the endless possibilities of your business or idea.
-              </p>
-              <Image
-                className='absolute -right-8 -bottom-8 lg:right-0 lg:bottom-0'
-                src={exploreImg}
-                alt='Image'
-              />
-            </motion.div>
-            <motion.div
-              className='bg-bv-black-1 overflow-hidden relative rounded-2xl px-[29px] pt-[40px] h-[200px] lg:h-[300px]'
-              variants={fadeInUp}
-            >
-              <p className='font-clash text-[24px] leading-[29.52px] lg:text-[32px] lg:leading-[39.36px]'>
-                Build your MVP in 6 weeks.
-              </p>
-              <Image
-                className='absolute -right-16 -bottom-24 lg:right-0 lg:bottom-0 lg:relative mx-auto'
-                src={mvpImg}
-                alt='Image'
-                width={186}
-                height={186}
-              />
-            </motion.div>
-          </motion.div>
-        </motion.div>
-      </section>
+        </section>
+      </Element>
       <section className='lg:px-[104px] px-[22px] pb-[77px] bg-white mt-[74px]'>
         <motion.h1
           className='font-clash text-[24px] lg:text-[48px] font-medium text-center text-bv-black-1'
